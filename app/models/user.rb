ea38c61be76e2740f8e01f814dd8d927fbd80d01
Hoang Phuong <phuong1492.bka@gmail.com>
2015-03-25 10:09:14 +0700

    show-edit-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :username,  presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, length: {minimum: 6},allow_blank: true
+
+  scope :without_user, ->user {where.not(id: user)}
+  
 end
