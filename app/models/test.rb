c1863275689fd0da9944b22e4fba100bbb45222d
Bui Minh Thu <buiminhthuk55@gmail.com>
2015-04-01 15:27:46 +0700

    update user answers

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,9 +2,26 @@ class Test < ActiveRecord::Base
   belongs_to :user
   belongs_to :lesson
   has_many :answers, dependent: :destroy
+
   accepts_nested_attributes_for :answers, allow_destroy: true
 
-  before_save :init_answers
+  before_create :init_answers
+
+  def completed?
+    self.status == "Completed"
+  end
+
+  def submitted?
+    self.status == "Submitted"
+  end
+
+  def ready?
+    self.status == "Ready"
+  end
+
+  def testing?
+    !self.ready? && !self.completed?
+  end
 
   private
   def init_answers
